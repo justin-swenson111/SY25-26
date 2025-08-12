45b2b9006264d6cf0aaf66346626d3956e6d7f58
justin <justinswenson111@gmail.com>
2025-08-12 11:35:58 -0700

    minor change

--- 2D game/new-game-project/scripts/character/mainControls.gd
+++ 2D game/new-game-project/scripts/character/mainControls.gd
@@ -1,11 +1,1 @@
 extends RigidBody2D
-
-
-func _on_body_entered(body: Node) -> void:
-	print("collided with" +body.name)
-	pass # Replace with function body.
-
-
-func _on_ready() -> void:
-	print("running")
-	pass # Replace with function body.
