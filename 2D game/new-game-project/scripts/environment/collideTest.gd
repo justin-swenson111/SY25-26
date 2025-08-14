0a2c06bed283daf80f039e85dbb7ba80f1027a59
justin <justinswenson111@gmail.com>
2025-08-14 11:51:47 -0700

    Player movement
    Collision with environment and basic scene change

--- 2D game/new-game-project/scripts/environment/collideTest.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-
-func _on_body_entered(body: Node2D) -> void:
-	print("collided with" +body.name)
-	pass # Replace with function body.
---
+++ multiProject/multiProject/First.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class First here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class First
+{
+    public static void main(String args[])
+    {
+        System.out.print("hello world");
+    }
+}
