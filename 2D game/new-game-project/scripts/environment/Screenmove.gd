8192eb612ed081e7f910059a8d18f30feead4ecf
justin <justinswenson111@gmail.com>
2025-08-15 10:24:22 -0700

    added scene movement

--- 2D game/new-game-project/scripts/environment/Screenmove.gd
+++ 2D game/new-game-project/scripts/environment/Screenmove.gd
@@ -3,4 +3,8 @@ extends Node
 
 func _on_body_entered(body: Node2D) -> void:
 	if (body.name=="CharacterBody2D"):
-		get_tree().change_scene_to_file("res://test2.tscn")
+		if (name=="LEdge"):
+			get_tree().change_scene_to_file("res://test2.tscn")
+			var mainChar = load("res://resources/Main.tscn");
+		if (name=="REdge"):
+			get_tree().change_scene_to_file("res://Test.tscn")
