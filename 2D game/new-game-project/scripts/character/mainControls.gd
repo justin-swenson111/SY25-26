a98828dffb6972cd45fa0364423c2f7e4d7adc5f
justin <justinswenson111@gmail.com>
2025-09-04 11:31:17 -0700

    added attack delay

--- 2D game/new-game-project/scripts/character/mainControls.gd
+++ 2D game/new-game-project/scripts/character/mainControls.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @export var speed = 400
 var dir = Vector2.UP
+var canAttack = true
 func get_input():
 	
 	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
@@ -12,15 +13,23 @@ func get_input():
 func _physics_process(_delta):
 	get_input()
 	move_and_slide()
-	if Input.is_action_just_pressed("attack"):
+	if Input.is_action_just_pressed("attack") && canAttack:
+		print("attack")
+		canAttack=false
 		var hurt = get_node(str(dir).replace(".","_"))
 		hurt.visible=true
 		hurt.collision_mask |= (1 << 1)
 		await get_tree().create_timer(0.25).timeout
 		stopAttack(hurt)
+		await get_tree().create_timer(0.5).timeout
+		attackDelay()
 		
 func stopAttack(hurt):
 	hurt.visible=false
 	hurt.collision_mask &= ~(1 << 1)
+	
+func attackDelay():
+	canAttack=true
+
 
 		
